d7c8b490f169048577e994fdd6c90a4c65a6533f
samuelcwilliams <samcwilliams67@gmail.com>
2022-07-06 21:47:57 -0500

    updated .gitignore

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 # View functions take a request and provide a response.
 # Could also be seen as request handlers.
+
+def say_hello(request):
+    return HttpResponse('initial view')
